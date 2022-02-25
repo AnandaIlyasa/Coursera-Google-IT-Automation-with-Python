a7c618eb0f9c662697b3ea8b21677dceca3493d7
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2022-02-25 21:22:06 +0800

    hapus unnecessary file

--- shebang.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-#the above line is shebang line that allows to execute this file with ./filename command
-#it adds 'python' command before the filename
-print("kode berhasil dieksekusi")
